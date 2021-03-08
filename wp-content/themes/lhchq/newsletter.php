b98a3f337cbacb6845ff4244bec7ccb097ce2b81
richard <rich@fohrcard.com>
2021-03-08 00:26:02 -0700

    added mailchimp embed

--- wp-content/themes/lhchq/newsletter.php
+++ wp-content/themes/lhchq/newsletter.php
@@ -22,7 +22,35 @@
 <?php get_header();?>
 
 
-<div class="container-fluid container-lhc lead-container">
+<div class="container-fluid" id="mailchimp-signup">
+  <div class="row">
+    <div class="col-8">
+      <!-- Begin Mailchimp Signup Form -->
+      <div id="mc_embed_signup">
+        <form action="https://lhchq.us17.list-manage.com/subscribe/post?u=d78e6378250fe9ea33d1d8c74&amp;id=9ff04b7d82" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+            	<label for="mce-EMAIL">Subscribe</label>
+              <div class="clear"></div>
+            	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d78e6378250fe9ea33d1d8c74_9ff04b7d82" tabindex="-1" value=""></div>
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+            </div>
+        </form>
+        <div class="clear"></div>
+      </div>
+
+      <!--End mc_embed_signup-->
+    </div>
+    <div class="col-4" id="newsletter-testimonial">
+      <div class="testimonial">"By far the best newsletter I get from a dog."</div>
+      <div class="source">- Janet Yellen</div>
+    </div>
+  </div>
+</div>
+
+
+<div class="container-fluid container-lhc lead-container newsletter-container">
   <div class="row">
     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
       <h1 class="sans">Newsletter.</h1>

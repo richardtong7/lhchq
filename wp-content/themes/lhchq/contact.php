d018c235607c6c34d83af9807ba6699364c7b35b
Rich Tong <richardtong7@gmail.com>
2021-03-14 16:12:00 -0600

    updates

--- wp-content/themes/lhchq/contact.php
+++ wp-content/themes/lhchq/contact.php
@@ -6,6 +6,17 @@
 <?php get_header();?>
 
 
+<div class="container-fluid container-lhc lead-container">
+  <div class="row">
+    <div class="col-12">
+      <h1 class="sans"><?php echo get_the_title(); ?>.</h1>
+      <div class="description">
+        <?php echo get_the_content(); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 

5ae1996f00a9856141d846552d4109e3de9c45ad
Rich Tong <richardtong7@gmail.com>
2021-03-15 23:12:14 -0600

    added socials

--- wp-content/themes/lhchq/template-parts/footer.php
+++ wp-content/themes/lhchq/template-parts/footer.php
@@ -14,4 +14,10 @@ $tagline   = get_bloginfo( 'description', 'display' );
 ?>
 </div> <!-- main -->
 <footer id="site-footer" class="site-footer" role="contentinfo">
+	<ul id="footer-socials" class="flex-container">
+		<li><a href="https://twitter.com/lhchq"><img src="https://lhchq.com/wp-content/uploads/2021/03/002-twitter.png" alt=""/></a></li>
+		<li><a href="https://www.facebook.com/lhchq/"><img src="https://lhchq.com/wp-content/uploads/2021/03/001-facebook.png" alt=""/></a></li>
+		<li><a href="https://www.instagram.com/lhc_ig/"><img src="https://lhchq.com/wp-content/uploads/2021/03/003-instagram.png" alt="" id="instagram"/></a></li>
+		<li><a href="https://www.linkedin.com/company/lighthouse-creative-group/"><img src="https://lhchq.com/wp-content/uploads/2021/03/004-linkedin.png" alt=""/></a></li>
+	</ul>
 </footer>

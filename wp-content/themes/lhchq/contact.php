c5dad186342f5f75645a82708ed4cf438325bf68
Rich Tong <richardtong7@gmail.com>
2021-03-16 13:30:09 -0600

    updated contact form

--- wp-content/themes/lhchq/contact.php
+++ wp-content/themes/lhchq/contact.php
@@ -10,8 +10,8 @@
   <div class="row">
     <div class="col-12">
       <h1 class="sans"><?php echo get_the_title(); ?>.</h1>
-      <div class="description">
-        <?php echo get_the_content(); ?>
+      <div id="contact-form">
+        <?php echo do_shortcode('[wpforms id="624"]'); ?>
       </div>
     </div>
   </div>

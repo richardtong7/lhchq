e76b069342c8c6d06d8860f5375190c6cac50c4b
Rich Tong <richardtong7@gmail.com>
2021-03-15 23:27:16 -0600

    added project subhead copy

--- wp-content/themes/lhchq/projects.php
+++ wp-content/themes/lhchq/projects.php
@@ -9,6 +9,9 @@
   <div class="row">
     <div class="col-12">
       <h1 class="sans"><?php echo get_the_title(); ?>.</h1>
+      <div class="description" style="margin-bottom: 25px;">
+        <?php echo get_the_content(); ?>
+      </div>
       <div class="description small" id="newsletter-listings">
         <?php if (have_rows('project')) { ?>
           <?php while (have_rows('project')): the_row();

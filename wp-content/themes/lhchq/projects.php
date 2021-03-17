f79fa3beb365af809dc7a52ae67991c7ddbefd06
Rich Tong <richardtong7@gmail.com>
2021-03-16 23:22:35 -0600

    updated projects styling

--- wp-content/themes/lhchq/projects.php
+++ wp-content/themes/lhchq/projects.php
@@ -9,9 +9,9 @@
   <div class="row">
     <div class="col-12">
       <h1 class="sans"><?php echo get_the_title(); ?>.</h1>
-      <div class="description" style="margin-bottom: 25px;">
+      <h2 class="sans projects" style="margin-bottom: 25px;">
         <?php echo get_the_content(); ?>
-      </div>
+      </h2>
       <div class="description small" id="newsletter-listings">
         <?php if (have_rows('project')) { ?>
           <?php while (have_rows('project')): the_row();
